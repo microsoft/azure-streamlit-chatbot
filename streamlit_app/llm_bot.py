1603d2f95c9f905e51275011978b002cab3f8f17
Brice Chung <brchun@microsoft.com>
2023-07-17 22:26:57 -0700

    first

---
+++ streamlit_app/llm_bot.py
@@ -0,0 +1,9 @@
+# Description: This file contains the logic for the LLM bot
+def dummy_bot(msg):
+    return "Dummy bot: I'm a dummy bot"
+
+def echo_bot(msg):
+    return 'Echo: {}'.format(msg)
+
+def simple_llm(msg):
+    return 'Simple LLM: {}'.format(msg)
---
+++ streamlit_app/main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import yaml
+from llm_bot import dummy_bot, echo_bot #contains logic for bot's response
+
+# Read config yaml file
+with open('./streamlit_app/config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+#print(config)
+title = config['streamlit']['title']
+avatar = {
+    'user': None,
+    'assistant': config['streamlit']['avatar']
+}
+
+# Set page config
+st.set_page_config(
+    page_title=config['streamlit']['tab_title'], 
+    page_icon=config['streamlit']['page_icon'], 
+    )
+
+# Set sidebar
+st.sidebar.title("About")
+st.sidebar.info(config['streamlit']['about'])
+
+# Set logo
+st.image(config['streamlit']['logo'], width=50)
+
+# Set page title
+st.title(title)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [] 
+    st.session_state.messages.append({
+        "role": "assistant", 
+        "content": config['streamlit']['assistant_intro_message']
+        })
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"], avatar=avatar[message["role"]]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Send a message"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Get bot response    
+    response = echo_bot(prompt)
+    with st.chat_message("assistant", avatar=config['streamlit']['avatar']):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+    
